baa84e80fb54d8edf4b09f388968368afbd098cc
actyvystom <thomas.goerldt@actyvyst.com>
2023-06-21 15:23:20 +0200

    try refactor HomePage

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,13 @@
+import TravelPlan from "../components/TravelPlan";
 import TravelPlanForm from "../components/TravelPlanForm";
-import TravelPlan from "../components/TravelPlan"; 
 
-
-const HomePage = ({travelPlans, onAddTravelPlan}) => {
-    return (
+export default function HomePage({ travelPlans, onAddTravelPlan }) {
+  return (
     <>
-        <TravelPlanForm addTravelPlan={onAddTravelPlan} />
-        {travelPlans.map((plan, index) => (
-            <TravelPlan key={index} plan={plan} />
-        ))}
+      <TravelPlanForm addTravelPlan={onAddTravelPlan} />
+      {travelPlans.map((plan, index) => (
+        <TravelPlan key={index} plan={plan} />
+      ))}
     </>
-    )
+  );
 }
-
-export default HomePage;
